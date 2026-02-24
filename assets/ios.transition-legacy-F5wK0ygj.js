73ab0f7344f3ce6c3946b0b2f6bfc9d60edfbe96
Srikanth <nandiraju@yahoo.com>
2026-02-24 12:54:33 -0800

    Updates

--- assets/ios.transition-legacy-F5wK0ygj.js
+++ assets/ios.transition-legacy-F5wK0ygj.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-B-nBNrB1.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-DlgqN0Ai.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
